fdde9412e6df5b6fea3bf708ef5b768b88b6f0ef
Myriam <ideasalcuadro@gmail.com>
2023-01-07 20:14:01 +0100

    ADD: generate password function

--- script.js
+++ script.js
@@ -89,11 +89,11 @@ let upperCasedCharacters = [
 ];
 
 // Variable Declaration
-let passwordLength;
-let includeLowercase;
-let includeUppercase;
-let includeNumeric;
-let includeSpecial;
+let passwordLength = 0;
+let includeLowercase = false;
+let includeUppercase = false;
+let includeNumeric = false;
+let includeSpecial = false;
 
 // Function to prompt user for password
 function getPasswordOptions() {
@@ -103,7 +103,7 @@ function getPasswordOptions() {
   );
 
   // Loop if answer is outside the parameters
-  while (passwordLength < 10 || passwordLength > 64) {
+  if (passwordLength < 10 || passwordLength > 64) {
     alert(
       "Password does not meet specified length requirement, please try again."
     );
@@ -113,61 +113,97 @@ function getPasswordOptions() {
     );
   }
   // Return back the total characters the password will have
-  alert(`Your password will have ${passwordLength} characters`);
+  alert(`Your password will have ${passwordLength} characters.`);
 
   //window.confirm for user to select password criteria with  boolean response for each option and variable declared to store input
   let includeLowercase = confirm(
-    "Would you like to use LOWERCASE characters?\nIf YES press ok, if NO press cancel.\nRemember that at least one type of character should be selected."
+    "1/4\nWould you like to use LOWERCASE characters?\nIf YES press ok, if NO press cancel.\nRemember that at least one type of character should be selected."
   );
   let includeUppercase = confirm(
-    "Would you like to use UPPERCASE characters?\nIf YES press ok, if NO press cancel.\nRemember that at least one type of character should be selected."
+    "2/4\nWould you like to use UPPERCASE characters?\nIf YES press ok, if NO press cancel.\nRemember that at least one type of character should be selected."
   );
   let includeNumeric = confirm(
-    "Would you like to use NUMERIC characters?\nIf YES press ok if, NO press cancel.\nRemember that at least one type of character should be selected."
+    "3/4\nWould you like to use NUMERIC characters?\nIf YES press ok if, NO press cancel.\nRemember that at least one type of character should be selected."
   );
   let includeSpecial = confirm(
-    "Would you like to use SPECIAL characters characters?\nIf YES press ok, if NO press cancel.\nRemember that at least one type of character should be selected."
+    "4/4\nWould you like to use SPECIAL characters characters?\nIf YES press ok, if NO press cancel.\nRemember that at least one type of character should be selected."
   );
 
   // Loop if the users's input is outside the parameters
-  while (
+  if (
     includeLowercase === false &&
     includeUppercase === false &&
     includeNumeric === false &&
     includeSpecial === false
   ) {
     alert("Remember that at least one type of character should be selected.");
-    includeLowercase = confirm(
+
+    let includeLowercase = confirm(
       "Would you like to use LOWERCASE characters?\nIf yes press ok if no press cancel.\nRemember that at least one type of character should be selected."
     );
-    includeUppercase = confirm(
+    let includeUppercase = confirm(
       "Would you like to use UPPERCASE characters?\nIf yes press ok if no press cancel.\nRemember that at least one type of character should be selected."
     );
-    includeNumeric = confirm(
+    let includeNumeric = confirm(
       "Would you like to use NUMERIC characters?\nIf yes press ok if no press cancel.\nRemember that at least one type of character should be selected."
     );
-    includeSpecial = confirm(
+    let includeSpecial = confirm(
       "Would you like to use SPECIAL characters characters?\nIf yes press ok if no press cancel.\nRemember that at least one type of character should be selected."
     );
   }
 
-  // console.log(passwordLength);
-  // console.log(includeLowercase);
-  // console.log(includeUppercase);
-  // console.log(includeNumeric);
-  // console.log(includeSpecial);
-
   // Return an object with the user's inputs.
+
   return {
-    passwordLength,
-    includeLowercase,
-    includeUppercase,
-    includeNumeric,
-    includeSpecial,
+    charactersLength: passwordLength,
+    lowercase: includeLowercase,
+    uppercase: includeUppercase,
+    numeric: includeNumeric,
+    special: includeSpecial,
   };
 }
 
 // Function for getting a random element from an array.
 function getRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  randomCharacter = arr[Math.floor(Math.random() * arr.length)];
+  return randomCharacter;
 }
+
+// Function to generate password with user input.
+function generatePassword() {
+  let userInput = getPasswordOptions();
+  // An empty array for the characters selected.
+  let arr = [];
+
+  if (userInput.lowercase) {
+    arr = arr.concat(lowerCasedCharacters);
+  }
+  if (userInput.uppercase) {
+    arr = arr.concat(upperCasedCharacters);
+  }
+  if (userInput.numeric) {
+    arr = arr.concat(numericCharacters);
+  }
+  if (userInput.special) {
+    arr = arr.concat(specialCharacters);
+  }
+  // An empty string for the passwords
+  let password = "";
+  for (let i = 0; i < userInput.charactersLength; i++) {
+    password = password.concat(getRandom(arr));
+  }
+  return password;
+}
+
+// Get references to the #generate element
+let generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
